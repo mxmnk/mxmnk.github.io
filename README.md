6e0520c2cb07c219f2c17785e1062988f6f5a03c
mvximenko <mr.mxmnk@gmail.com>
2020-04-06 20:37:58 +0900

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# mxmnk.github.io
---
+++ precache-manifest.15c510c3f7b32279c200ab3ee3cfbd4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3a048d8f271a30affeb502ee5287ca",
+    "url": "/mxmnk.github.io/wordy/index.html"
+  },
+  {
+    "revision": "f4096118f4fd5f459822",
+    "url": "/mxmnk.github.io/wordy/static/css/main.1144d03e.chunk.css"
+  },
+  {
+    "revision": "e6636499a780a8983a49",
+    "url": "/mxmnk.github.io/wordy/static/js/2.589c9cf4.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/mxmnk.github.io/wordy/static/js/2.589c9cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4096118f4fd5f459822",
+    "url": "/mxmnk.github.io/wordy/static/js/main.244ad322.chunk.js"
+  },
+  {
+    "revision": "6be746382ff98c7ac60e",
+    "url": "/mxmnk.github.io/wordy/static/js/runtime-main.cd860875.js"
+  }
+]);
